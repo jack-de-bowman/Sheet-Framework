94d2fffa0bf0974bdcc9eb7d0b1eea5fef2a30c1
Jacques Dubuc <jacquesdubuc3@gmail.com>
2025-07-30 10:09:01 -0400

    rename commit

--- main/main_module.py
+++ main/main_module.py
@@ -1,1 +1,9 @@
-#HELLO
+class Widgets_Manager:
+	def frames_constructoor(self, widgets_dict):
+		created_widgets= {}
+		for widgets_name, options in widgets_dict.items():
+			parent = options.pop("parent")
+			grid_options = {k:options.pop(k) for k in ["row","column","sticky"] if k in options}
+			frame = tk.Frame(parent, **options)
+			frame.grid(**grid_options)
+			created_widgets[widget_name] = widget
